3985a2436457c752b8caca2591819a90c96ce8ef
tommasosciessere <147245640+tommasosciessere@users.noreply.github.com>
2023-12-02 11:42:15 +0000

    Beta 3

--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -6,24 +6,37 @@
     <title>Form informazioni</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
-    <form action="./accesso_pagina_benvenuto.php">
+<body style="background-color: #87ddee;">
+<table>
+    <center>
+    <td>
+    <center>
+        <div style="background-color: 	#4d99ea; margin: 150px; border-radius: 15px;"><br>
+        <h1>Login ID Apple</h1><br>
+        <h4>Usa un dispositivo con iOS 17, iPadOS 17, macOS Sonoma o successivo. In alternativa inserisci le credenziali</h2><br>
+<form action="./accesso_pagina_benvenuto.php">
         <label for="Nome">Inserire nome: </label>
-        <input type="text" id="Nome" name="Nome"><br>
+        <input type="text" id="Nome" name="Nome"><br><br>
         <label for="Cognome">Inserire cognome: </label>
-        <input type="text" id="Cognome" name="Cognome"><br>
+        <input type="text" id="Cognome" name="Cognome"><br><br>
         <label for="Età">Inserire età: </label>
-        <input type="number" id="Età" name="Età"><br>
+        <input type="number" id="Età" name="Età"><br><br>
         <label for="Sport">Inserire sport preferito: </label>
-        <input type="text" id="Sport" name="Sport"><br>
+        <input type="text" id="Sport" name="Sport"><br><br>
         <label for="Colore">Inserire colore interfaccia: </label>
-        <input type="color" id="Colore" name="Colore"><br>
+        <input type="color" id="Colore" name="Colore"><br><br>
         <label for="Password">Inserire Password: </label>
-        <input type="password" id="Password" name="Password"><br>
-        <input type="submit">
-
-
+        <input type="password" id="Password" name="Password"><br><br>
+        <input type="submit"><br><br>
     </form>
+    </div>
+    </center>
+    </td>
+    <td style="width: 50%;">
+    <img src="https://exnovocomputer.it/media/catalog/product/cache/b735f906b33c7889668f7237dab85a6d/a/p/apple-iphone-12mini-allcolors.png" alt="no" style="width: 900px;">
+    </td>
+    </center>
+    </table>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
