7d13385dcc1a1f550283da045be71042d95cef6d
tommasosciessere <147245640+tommasosciessere@users.noreply.github.com>
2023-12-02 14:26:09 +0000

    Release Candidate

--- esercizi/questionario/accesso_pagina_benvenuto.php
+++ esercizi/questionario/accesso_pagina_benvenuto.php
@@ -6,39 +6,146 @@
     <title>Form informazioni</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body style="background-color: <?=$_GET['Colore']?>">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="https://images.vexels.com/media/users/3/129234/isolated/lists/73970c892d748c7507db8e10d71535b0-apple-logo-icon.png" style="width: 40px;">
+</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.apple.com/it/store">Store</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/mac/">Mac</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/ipad/">iPad</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/iphone/">iPhone</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/watch/">Watch</a>
+          <li class="nav-item">
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/apple-vision-pro/">Vision</a>
+        </li>
+          <a class="nav-link" href="https://www.apple.com/it/airpods/">AirPods</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/tv-home/">TV & Casa</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/services/">Intrattenimento</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/shop/accessories/all">Accessori</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://support.apple.com/it-it">Supporto</a>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Altro
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">La storia di Apple</a></li>
+            <li><a class="dropdown-item" href="#">I nostri software</a></li>
+            <li><a class="dropdown-item" href="#">La nostra sede</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body style="background-color: <?=$_GET['Colore']?>;"><br>
+<center>
     <h1>Benvenuto 
     <?=$_GET['Nome']?>
-    <?=$_GET['Cognome']?>
-    </h1>
-    <table class="table">
+    <?=$_GET['Cognome']?>!
+    </h1><br>
+    <table  style="margin: 30px;">
+        <td>
+            <center>
+                <div style="margin: 20px;">
+    <table class="table table-secondary">
 <tbody>
-<tr>
-<td>Nome</td>
-<td><?=$_GET['Nome']?></td>
-</tr>
-<tr>
-<td>Cognome</td>
-<td><?=$_GET['Cognome']?></td>
-</tr>
-<tr>
-<td>Età</td>
-<td><?=$_GET['Età']?></td>
-</tr>
-<tr>
-<td>Sport preferito</td>
-<td><?=$_GET['Sport']?></td>
-</tr>
-<tr>
-<td>Colore</td>
-<td><?=$_GET['Colore']?></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><?=$_GET['Password']?></td>
-</tr>
-    </tbody>
+    <tr>
+    <td><b>Nome</b></td>
+    <td><?=$_GET['Nome']?></td>
+    </tr>
+    <tr>
+    <td><b>Cognome</b></td>
+    <td><?=$_GET['Cognome']?></td>
+    </tr>
+    <tr>
+    <td><b>Età</b></td>
+    <td><?=$_GET['Età']?></td>
+    </tr>
+    <tr>
+    <td><b>Sport preferito</b></td>
+    <td><?=$_GET['Sport']?></td>
+    </tr>
+    <tr>
+    <td><b>Colore</b></td>
+    <td><?=$_GET['Colore']?></td>
+    </tr>
+    <tr>
+    <td><b>Password</b></td>
+    <td><?=$_GET['Password']?></td>
+    </tr>
+</tbody>
 </table>
+</div>
+</center>
+</td>
+<td style="width:50%">
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.smartworld.it/images/2022/10/11/facetime-macos_880x495.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://techhub.osu.edu/sites/default/files/products/ipad_mini_wi-fi_lineup_screen_usen-2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.docomo.ne.jp/flcache_data/english/iphone/images/12/img_main_smt.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</td>
+</table>
+</center>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
--- esercizi/questionario/index.php
+++ esercizi/questionario/index.php
@@ -6,12 +6,69 @@
     <title>Form informazioni</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="https://images.vexels.com/media/users/3/129234/isolated/lists/73970c892d748c7507db8e10d71535b0-apple-logo-icon.png" style="width: 40px;">
+</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.apple.com/it/store">Store</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/mac/">Mac</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/ipad/">iPad</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/iphone/">iPhone</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/watch/">Watch</a>
+          <li class="nav-item">
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/apple-vision-pro/">Vision</a>
+        </li>
+          <a class="nav-link" href="https://www.apple.com/it/airpods/">AirPods</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/tv-home/">TV & Casa</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/services/">Intrattenimento</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://www.apple.com/it/shop/accessories/all">Accessori</a>
+          <li class="nav-item">
+          <a class="nav-link" href="https://support.apple.com/it-it">Supporto</a>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Altro
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">La storia di Apple</a></li>
+            <li><a class="dropdown-item" href="#">I nostri software</a></li>
+            <li><a class="dropdown-item" href="#">La nostra sede</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <body style="background-color: #87ddee;">
 <table>
     <center>
     <td>
     <center>
-        <div style="background-color: 	#4d99ea; margin: 150px; border-radius: 15px;"><br>
+        <div style="background-color: 	#4d99ea; margin: 100px; border-radius: 15px;"><br>
         <img src="https://www.icloud.com/system/icloud.com/2403Hotfix31/it-it/4f72d89d71e9abcc4e37c71fb77fe65b.svg" alt="ID Apple" style="width: 160px;">
         <h1>Login ID Apple</h1><br>
         <h5>Usa un dispositivo con iOS 17, iPadOS 17, macOS Sonoma o successivo. In alternativa inserisci le credenziali. I dispositivi Android non sono accettati.</h2><br>
@@ -34,7 +91,7 @@
     </center>
     </td>
     <td style="width: 50%;">
-    <video width="920" style="border-radius: 15px;" controls>
+    <video width="700" style="border-radius: 15px;" controls>
   <source src="https://www.icloud.com/system/icloud.com/2403Hotfix31/fpo@1x.mp4" type="video/mp4">
 </video>
     </td>
